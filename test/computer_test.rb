22c07d33ed1b7230d73add71f7eb1e35d33dbca7
bghalami <benjaminghalami@gmail.com>
2018-07-16 15:37:33 -0600

    finished computer test

--- test/computer_test.rb
+++ test/computer_test.rb
@@ -6,7 +6,14 @@ class ComputerTest < Minitest::Test
 
   def test_it_exists
     computer = Computer.new
-     assert_instance_of Computer, computer
+    assert_instance_of Computer, computer
+  end
+
+  def test_it_generates_random_number_in_range
+    computer = Computer.new
+    tester = [computer.make_move]
+
+    assert true, tester.include?(0..6)
   end
 
 end
