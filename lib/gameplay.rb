d01f64ccd855e3d66bb3ac2143ff3bfbc2acac2a
bghalami <benjaminghalami@gmail.com>
2018-07-17 18:48:22 -0600

    committing after like 3 hours...

--- lib/gameplay.rb
+++ lib/gameplay.rb
@@ -50,4 +50,80 @@ class GamePlay
     @game_board.see_board
   end
 
+  def horizontal_checker(row)
+    if row > 0
+      check = @game_board.board[row].join
+      if check.include?("XXXX")
+        "X's Win!!!"
+      elsif
+        check.include?("OOOO")
+          "O's Win!!!"
+      else
+        horizontal_checker(row - 1)
+      end
+    end
+  end
+
+  def vertical_checker(column)
+    if column < 7
+      column_array = @game_board.board.map do |row|
+        row[column]
+      end
+      check = column_array.join
+      if check.include?("XXXX")
+        "X's Win!!!"
+      elsif
+      check.include?("OOOO")
+        "O's Win!!!"
+      else
+        vertical_checker(column + 1)
+      end
+    end
+  end
+
+  def diagonal_checker(row, column)
+
+
+  end
+
+  def bl_tr_checker(row,column)
+    if column < 4
+      array = []
+      array << @game_board.board[row][column]
+      array << @game_board.board[row - 1][column + 1]
+      array << @game_board.board[row - 2][column + 2]
+      array << @game_board.board[row - 3][column + 3]
+      check = array.join
+      if check.include?("XXXX")
+        "X's Win!!!"
+      elsif
+        check.include?("OOOO")
+        "O's Win!!!"
+      else
+        bl_tr_checker(row,column + 1)
+      end
+    end
+  end
+
+  def ultra_checker(row,column)
+    if row > 0
+      sin = bl_tr_checker(row,column)
+      if sin == "X's Win!!!" || sin == "O's Win!!!"
+        "Now we will kill the weak"
+      else
+        ultra_checker(row - 1, column)
+      end
+    end
+  end
+
+
 end
+
+gp = GamePlay.new
+
+gp.game_board.board[5][1] = "O"
+gp.game_board.board[4][2] = "O"
+gp.game_board.board[3][3] = "O"
+gp.game_board.board[2][4] = "O"
+
+binding.pry
